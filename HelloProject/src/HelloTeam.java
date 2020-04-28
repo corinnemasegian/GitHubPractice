a5c0d3846c9eddd545f7a546ca3b6b7e2659e24c
annaha0618 <54450898+annaha0618@users.noreply.github.com>
2020-04-28 14:01:25 -0700

    Update HelloTeam.java

--- HelloProject/src/HelloTeam.java
+++ HelloProject/src/HelloTeam.java
@@ -4,11 +4,11 @@ public class HelloTeam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings, Mr. Turner!");
-		System.out.println("Hello, Zachery!");
+		System.out.println("Hello, Branham!");
 		System.out.println("Hello students!");
 		System.out.println("Salutations to Mr. Turner's new room J205!");
-		System.out.println("Ben Marty");
 		System.out.println("Aayushi Neema");
+		System.out.println("Anna Ha");
 	}
 
 }
